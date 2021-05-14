6116c385b75a63ecad8bc98ba3d20a627b27e302
CHOChangHun <cvcvcx9@gmail.com>
2021-05-14 15:49:29 +0900

    053문제 실습

--- 053/Program.cs
+++ 053/Program.cs
@@ -6,7 +6,21 @@ namespace _053
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("숫자를 입력하세요");
+            double n = double.Parse(Console.ReadLine());
+
+            double sum = 0;
+            for(double i =1;i<= n; i++)
+            {
+                double fact = 1;
+                for(double j = 2; j<=i; j++)
+                {
+                    fact *= j;
+                }
+                sum += fact;
+                Console.WriteLine("{0,2}! = {1,10:#,#}",i,fact);
+            }
+            Console.WriteLine("1! + 2! + ... + {0}! = {1:n0}\n",n,sum);
         }
     }
 }
